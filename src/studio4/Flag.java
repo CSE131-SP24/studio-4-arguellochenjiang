dcfe08e8ff5ea9cb3ee10074c8ba79d8c4857b9e
frankj <frankj@10.232.197.226>
2024-02-21 16:46:21 -0600

    FINAL FREEDOM FLAG

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,25 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		
+		
+		double freedomFlag = Math.random()*0.25 + 0.15;
+		
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(0.5, 0.5, freedomFlag);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledRectangle(0.5, 0.5, 0.15, 0.1);
+		
+		//make an outline of a rectangle
+		StdDraw.rectangle(0.5, 0.5, freedomFlag+0.12, freedomFlag+0.1);
+		
+		
 		
 	}
 }
